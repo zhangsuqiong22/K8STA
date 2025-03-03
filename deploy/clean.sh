9dcd6dd5374f7febfefce4d826f2cb70bac56a5e
zhangsuqiong22 <129636559+zhangsuqiong22@users.noreply.github.com>
2025-03-03 19:18:57 +0800

    update clean env scripts

--- deploy/clean.sh
+++ deploy/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Delete testing-related Kubernetes resources
 kubectl delete deployment.apps/tester-testing-report-manager -n kubeta
 kubectl delete deployment.apps/case-controller -n kubeta
 kubectl delete svc tester-testing-report-manager -n kubeta
